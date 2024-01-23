4c4cc2bd47053f7f3ef7f2de4bcdd91c7f7029c9
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2024-01-23 21:19:25 +0200

    bug fixed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,5 @@
 import { Container } from 'components/Container/Container';
+import { GoBackLink } from 'components/GoBackLink/GoBackLink.styled';
 import { ImageMovie } from 'components/MovieCard/MovieCard.styled';
 import image from 'images/not-found.jpg';
 
@@ -7,6 +8,10 @@ const NotFound = () => {
     <main>
       <section>
         <Container>
+          <GoBackLink to="/">
+            <p>To home page</p>
+          </GoBackLink>
+
           <ImageMovie src={image} alt="404 Not found" />
         </Container>
       </section>
